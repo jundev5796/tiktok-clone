99954c84c1047946446e54009b9a60f1895e10db
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-20 15:05:44 +0900

    Implemented code for sizes and gaps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -6,7 +8,6 @@ void main() {
 
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +15,16 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(children: [
+            Text('hello'),
+            Gaps.h20,
+            Text('hello'),
+          ]),
+        ),
+      ),
     );
   }
 }
