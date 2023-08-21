6c05b7f5b110c3d3df4073322b8323b37c331fb8
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-21 21:34:49 +0900

    Added themes for reusable widgets on main

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
 
 class EmailScreen extends StatelessWidget {
   const EmailScreen({super.key});
@@ -9,15 +8,8 @@ class EmailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
-        elevation: 0,
         title: const Text(
           "Sign up",
-          style: TextStyle(
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-          ),
         ),
       ),
     );
