75daf6cc9465bffd098e0ab8bc7690776f04a9ac
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-21 22:02:00 +0900

    [Username] Rendered login field and button

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -14,13 +14,13 @@ class UsernameScreen extends StatelessWidget {
           "Sign up",
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: Sizes.size36),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Gaps.v40,
-            Text(
+            const Text(
               "Create username",
               style: TextStyle(
                 fontSize: Sizes.size24,
@@ -28,11 +28,48 @@ class UsernameScreen extends StatelessWidget {
               ),
             ),
             Gaps.v8,
-            Text(
+            const Text(
               "You can always change this later.",
               style: TextStyle(
                 fontSize: Sizes.size16,
                 color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Username",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size16,
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: const Text(
+                  'Next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
             ),
           ],
