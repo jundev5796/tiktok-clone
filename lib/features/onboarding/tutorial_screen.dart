970e7c02ac258b6494cce1ee591a78913294a727
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-24 15:25:40 +0900

    [Tutorial Screen] Rendered texts

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,6 +12,33 @@ class TutorialScreen extends StatefulWidget {
 class _TutorialScreenState extends State<TutorialScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: Column(
+            children: [
+              Gaps.v52,
+              Text(
+                "Watch cool videos!",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v16,
+              Text(
+                "Videos are personalized for you based on what you watch, like, and share.",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
