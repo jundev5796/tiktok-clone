23e09be59a1a2e63d2e044196f0198ac9bba3a98
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-24 12:44:40 +0900

    [Interest Screen] Rendered title and subtitle

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Choose your interests"),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: Sizes.size24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v32,
+            Text(
+              "Choose your interests",
+              style: TextStyle(
+                fontSize: Sizes.size40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Gaps.v20,
+            Text(
+              "Get better video recommendations",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
