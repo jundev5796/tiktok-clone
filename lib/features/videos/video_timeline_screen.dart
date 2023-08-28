d37d477f373b3275f29f58ae714a65bf3b9dc90e
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-28 12:19:36 +0900

    [Video Screen] Video screen start

---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
