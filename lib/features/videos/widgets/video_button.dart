0804d1237e921a51a827e5dc970950588b33c168
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-29 14:35:13 +0900

    [Home Screen] rendered buttons

--- lib/features/videos/widgets/video_button.dart
+++ lib/features/videos/widgets/video_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoButton extends StatelessWidget {
   final IconData icon;
@@ -16,9 +17,19 @@ class VideoButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FaIcon(icon),
-        Gaps.v10,
-        Text(text),
+        FaIcon(
+          icon,
+          color: Colors.white,
+          size: Sizes.size40,
+        ),
+        Gaps.v5,
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ],
     );
   }
