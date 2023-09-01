32c066b3d2d59c78a9d6d28fc7dac20a4ff25ede
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-01 13:54:42 +0900

    [Inbox Screen] rendered header

--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
@@ -10,11 +11,30 @@ class InboxScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
         title: const Text("Inbox"),
         actions: [
           IconButton(
             onPressed: _onDmPressed,
             icon: const FaIcon(FontAwesomeIcons.paperPlane),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text(
+              "Activity",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size18,
+              ),
+            ),
+            trailing: FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+              color: Colors.black,
+            ),
           ),
         ],
       ),
