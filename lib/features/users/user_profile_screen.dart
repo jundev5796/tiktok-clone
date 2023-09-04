ef6ef625719f8701760c3ec91660983e38fccefd
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-04 14:43:00 +0900

    [Profile Screen] completed profile screen

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/users/widgets/persistent_tabbar.dart';
 import 'package:tiktok_clone/features/users/widgets/user_count.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -14,194 +15,172 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              title: const Text("Jun"),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const FaIcon(
-                    FontAwesomeIcons.gear,
-                    size: Sizes.size20,
-                  ),
-                ),
-              ],
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  const CircleAvatar(
-                    radius: 50,
-                    foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/69138182?v=4"),
-                    child: Text("Jun"),
-                  ),
-                  Gaps.v20,
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "@Jun",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: Sizes.size18,
-                        ),
-                      ),
-                      Gaps.h5,
-                      FaIcon(
-                        FontAwesomeIcons.solidCircleCheck,
-                        size: Sizes.size16,
-                        color: Colors.blue,
-                      ),
-                    ],
-                  ),
-                  Gaps.v24,
-                  SizedBox(
-                    height: Sizes.size48,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const UserCount(followerCount: "97", text: "Following"),
-                        VerticalDivider(
-                          width: Sizes.size32,
-                          thickness: Sizes.size1,
-                          color: Colors.grey.shade400,
-                          indent: Sizes.size14,
-                          endIndent: Sizes.size14,
-                        ),
-                        const UserCount(
-                            followerCount: "10M", text: "Followers"),
-                        VerticalDivider(
-                          width: Sizes.size32,
-                          thickness: Sizes.size1,
-                          color: Colors.grey.shade400,
-                          indent: Sizes.size14,
-                          endIndent: Sizes.size14,
-                        ),
-                        const UserCount(followerCount: "194.3M", text: "Likes"),
-                      ],
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: const Text("Jun"),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const FaIcon(
+                      FontAwesomeIcons.gear,
+                      size: Sizes.size20,
                     ),
                   ),
-                  Gaps.v14,
-                  FractionallySizedBox(
-                    widthFactor: 0.33,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: Sizes.size12,
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      foregroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/69138182?v=4"),
+                      child: Text("Jun"),
+                    ),
+                    Gaps.v20,
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "@Jun",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: Sizes.size18,
+                          ),
+                        ),
+                        Gaps.h5,
+                        FaIcon(
+                          FontAwesomeIcons.solidCircleCheck,
+                          size: Sizes.size16,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
+                    Gaps.v24,
+                    SizedBox(
+                      height: Sizes.size48,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const UserCount(
+                              followerCount: "97", text: "Following"),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          const UserCount(
+                              followerCount: "10M", text: "Followers"),
+                          VerticalDivider(
+                            width: Sizes.size32,
+                            thickness: Sizes.size1,
+                            color: Colors.grey.shade400,
+                            indent: Sizes.size14,
+                            endIndent: Sizes.size14,
+                          ),
+                          const UserCount(
+                              followerCount: "194.3M", text: "Likes"),
+                        ],
                       ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(Sizes.size4),
+                    ),
+                    Gaps.v14,
+                    FractionallySizedBox(
+                      widthFactor: 0.33,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: Sizes.size12,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(Sizes.size4),
+                          ),
+                        ),
+                        child: const Text(
+                          "Follow",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
                       ),
-                      child: const Text(
-                        "Follow",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
+                    ),
+                    Gaps.v14,
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Sizes.size32,
+                      ),
+                      child: Text(
+                        "Check out my content and follow my page if you want to support!",
                         textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
-                  Gaps.v14,
-                  const Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: Sizes.size32,
-                    ),
-                    child: Text(
-                      "Check out my content and follow my page if you want to support!",
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Gaps.v14,
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.link,
-                        size: Sizes.size12,
-                      ),
-                      Gaps.h4,
-                      Text(
-                        "https://https://github.com/jundev5796",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
+                    Gaps.v14,
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.link,
+                          size: Sizes.size12,
                         ),
-                      ),
-                    ],
-                  ),
-                  Gaps.v20,
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.symmetric(
-                        horizontal: BorderSide(
-                          color: Colors.grey.shade200,
-                          width: 0.5,
-                        ),
-                      ),
-                    ),
-                    child: const TabBar(
-                      indicatorSize: TabBarIndicatorSize.label,
-                      indicatorColor: Colors.black,
-                      labelPadding: EdgeInsets.symmetric(
-                        vertical: Sizes.size10,
-                      ),
-                      labelColor: Colors.black,
-                      tabs: [
-                        Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: Sizes.size20),
-                          child: Icon(Icons.grid_4x4_rounded),
-                        ),
-                        Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: Sizes.size20),
-                          child: FaIcon(FontAwesomeIcons.heart),
+                        Gaps.h4,
+                        Text(
+                          "https://https://github.com/jundev5796",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Gaps.v20,
+                  ],
+                ),
               ),
-            )
-          ];
-        },
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 20,
-              padding: EdgeInsets.zero,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: Sizes.size2,
-                mainAxisSpacing: Sizes.size2,
-                childAspectRatio: 9 / 14,
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: 9 / 14,
-                    child: FadeInImage.assetNetwork(
-                      fit: BoxFit.cover,
-                      placeholder: "assets/images/placeholder.jpeg",
-                      image:
-                          "https://e1.pxfuel.com/desktop-wallpaper/765/617/desktop-wallpaper-cool-anime-iphone-cool-iphone-thumbnail.jpg",
+            ];
+          },
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 20,
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size2,
+                  mainAxisSpacing: Sizes.size2,
+                  childAspectRatio: 9 / 14,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 14,
+                      child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/placeholder.jpeg",
+                        image:
+                            "https://e1.pxfuel.com/desktop-wallpaper/765/617/desktop-wallpaper-cool-anime-iphone-cool-iphone-thumbnail.jpg",
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Center(
-              child: Text('Page Two'),
-            ),
-          ],
+              const Center(
+                child: Text('Page Two'),
+              ),
+            ],
+          ),
         ),
       ),
     );
