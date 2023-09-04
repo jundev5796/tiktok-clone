7e0de4f9f26815fca8f3b785b6ef700fcd9c8b0d
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-04 12:11:53 +0900

    [Profile Screen] rendered headers and profile avatar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -11,8 +12,33 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [],
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: const Text("Jun"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const FaIcon(
+                FontAwesomeIcons.gear,
+                size: Sizes.size20,
+              ),
+            ),
+          ],
+        ),
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/69138182?v=4"),
+                child: Text("Jun"),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
