f270301ac2630398aa2e51ba72ad4f46d7899234
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-04 12:14:55 +0900

    [Profile Screen] rendered profile address

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -34,6 +35,25 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 foregroundImage: NetworkImage(
                     "https://avatars.githubusercontent.com/u/69138182?v=4"),
                 child: Text("Jun"),
+              ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "@Jun",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size18,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    size: Sizes.size16,
+                    color: Colors.blue,
+                  ),
+                ],
               ),
             ],
           ),
