aac871fb068d52d47e0ffb55c8cb5bf07f015c45
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-06 05:08:35 +0900

    removed debug tag on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
