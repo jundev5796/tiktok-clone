8e90199aa88124ec22d3acbdea3a550ebdc7159c
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-05 14:17:32 +0900

    [Settings Screen] implement fixes

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -9,9 +10,8 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  bool _notifications = true;
-
-  void _onNotificationChanged(bool? newValue) {
+  bool _notifications = false;
+  void _onNotificationsChanged(bool? newValue) {
     if (newValue == null) return;
     setState(() {
       _notifications = newValue;
@@ -22,20 +22,22 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Settings"),
+        title: const Text('Settings'),
       ),
       body: ListView(
         children: [
           SwitchListTile.adaptive(
             value: _notifications,
-            onChanged: _onNotificationChanged,
+            onChanged: _onNotificationsChanged,
             title: const Text("Enable notifications"),
+            subtitle: const Text("They will be cute."),
           ),
           CheckboxListTile(
             activeColor: Colors.black,
             value: _notifications,
-            onChanged: _onNotificationChanged,
-            title: const Text("Enable notification"),
+            onChanged: _onNotificationsChanged,
+            title: const Text("Marketing emails"),
+            subtitle: const Text("We won't spam you."),
           ),
           ListTile(
             onTap: () async {
@@ -58,11 +60,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 builder: (context, child) {
                   return Theme(
                     data: ThemeData(
-                      appBarTheme: const AppBarTheme(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.black,
-                      ),
-                    ),
+                        appBarTheme: const AppBarTheme(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black)),
                     child: child!,
                   );
                 },
@@ -71,6 +71,54 @@ class _SettingsScreenState extends State<SettingsScreen> {
             },
             title: const Text("What is your birthday?"),
             subtitle: const Text("I need to know!"),
+          ),
+          ListTile(
+            title: const Text("Log out (iOS)"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoDialog(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: const Text("Are you sure?"),
+                  content: const Text("Plx dont go"),
+                  actions: [
+                    CupertinoDialogAction(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("No"),
+                    ),
+                    CupertinoDialogAction(
+                      onPressed: () => Navigator.of(context).pop(),
+                      isDestructiveAction: true,
+                      child: const Text("Yes"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("Log out (Android)"),
+            textColor: Colors.red,
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  icon: const FaIcon(FontAwesomeIcons.skull),
+                  title: const Text("Are you sure?"),
+                  content: const Text("Plx dont go"),
+                  actions: [
+                    IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const FaIcon(FontAwesomeIcons.car),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("Yes"),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
           ListTile(
             title: const Text("Log out (iOS / Bottom)"),
@@ -97,7 +145,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
               );
             },
           ),
-          const AboutListTile(),
+          const AboutListTile(
+            applicationVersion: "1.0",
+            applicationLegalese: "Don't copy me.",
+          ),
         ],
       ),
     );
