a82d28eba2abd2df40c9b2d8b2941d2608ba6a92
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-15 00:35:19 +0900

    [MVVM with Provider] created model for autoplay

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -14,6 +14,7 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   bool _notifications = false;
+
   void _onNotificationsChanged(bool? newValue) {
     if (newValue == null) return;
     setState(() {
