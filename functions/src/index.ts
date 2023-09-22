dd9b58e49e64e7aa5146bc2f89a5ca3040064e73
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-22 18:34:54 +0900

    implemented ffmpeg format

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,17 @@ admin.initializeApp();
 export const onVideoCreated = functions.firestore
   .document("videos/{videoId}")
   .onCreate(async (snapshot, context) => {
-    snapshot.ref.update({ hello: "from functions" });
+    const spawn = require("child-process-promise").spawn;
+    const video = snapshot.data();
+    await spawn("ffmpeg", [
+      "-i",
+      video.fileUrl,
+      "-ss",
+      "00:00:01.000",
+      "-vframes",
+      "1",
+      "-vf",
+      "scale=150:-1",
+      `/tmp/${snapshot.id}.jpg`,
+    ]);
   });
