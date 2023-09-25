2bab71b56225fb4fe370e0d988a85da5eab93eb2
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-25 10:36:51 +0900

    [Direct Messages] implemented MVVM files

---
+++ lib/features/inbox/repos/messages_repo.dart
@@ -0,0 +1,23 @@
+import 'dart:js_interop';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/inbox/models/message_model.dart';
+
+class MessagesRepo {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> sendMessage(MessageModel message) async {
+    await _db
+        .collection("chat_rooms")
+        .doc("b342GRzrzCQioqyQAziI")
+        .collection("texts")
+        .add(
+          message.toJson(),
+        );
+  }
+}
+
+final messagesRepo = Provider(
+  (ref) => MessagesRepo(),
+);
---
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone/features/inbox/models/message_model.dart';
+import 'package:tiktok_clone/features/inbox/repos/messages_repo.dart';
+
+class MessageViewModel extends AsyncNotifier<void> {
+  late final MessagesRepo _repo;
+
+  @override
+  FutureOr<void> build() {
+    _repo = ref.read(messagesRepo);
+  }
+
+  Future<void> sendMessage(String text) async {
+    final user = ref.read(authRepo).user;
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final message = MessageModel(
+        text: text,
+        userId: user!.uid,
+      );
+      _repo.sendMessage(message);
+    });
+  }
+}
